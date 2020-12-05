091272e3dcdfa89bc0740fbd258a907781e0547d
shashank-somaraju <shashank1151999@gmail.com>
2020-12-05 23:52:59 +0530

    tempory cahnfge

---
+++ Animal.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Animal{
+      public int height;
+      public String color;
+      public void bark(String s){
+        String s1="dog";
+        if(s1.equals(s)){
+           System.out.println("it barks");
+        }
+        else{
+           System.out.println("it dosent bark");
+        }
+      }
+
+}
